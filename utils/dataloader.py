c57472b2eb0fb378e596a4b0f5fee2fca1548f4c
“FranceQA” <fabianqa97@hotmail.com>
2022-09-10 17:14:50 -0600

    Prueba 4

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -29,6 +29,22 @@ def load_data(path):
     img1_paths = [x.replace('flow.flo', 'img2.tif') for x in flo_paths]
     return flo_paths, img0_paths, img1_paths
 
+def load_data_roya(path):
+  videos = glob(os.path.join('/content/videos/'+'*'))
+
+  flo, img0, img1 = [], [], []
+  for video in videos:
+    img_paths = glob(os.path.join(video,'ima/*.png')); img_path.sort()
+    img0_paths = img_paths[0:-1]
+    img1_paths = img_paths[1:]
+    flo_paths = [x.replace('.png', '.flo') for x in img0_paths]
+    flo_paths = [x.replace('ima', 'flow') for x in flo_paths]
+
+    flo = flo + flo_paths
+    img0 = img0 + img0_paths
+    img1 = img1 + img0_paths
+
+  return flo, img0, img1
 
 class MyDataset(Dataset):
     def __init__(self, path,  transform=None):
@@ -39,6 +55,25 @@ class MyDataset(Dataset):
         img1 = cv.imread(self.img0_paths[i])
         img2 = cv.imread(self.img1_paths[i])
         flo = readFlowFile(self.flo_paths[i])
+        if self.transform is not None:
+            img1,img2,flo = self.transform(img1,img2,flo)
+        return img1, img2, flo
+
+    def __len__(self):
+        return len(self.img0_paths)
+
+class MyDataset_roya(Dataset):
+    def __init__(self, path,  transform=None):
+        self.flo_paths, self.img0_paths, self.img1_paths = load_data_roya(path)
+        self.transform = transform
+
+    def __getitem__(self, i):
+        img1 = cv.imread(self.img0_paths[i])
+        img2 = cv.imread(self.img1_paths[i])
+        flo = readFlowFile(self.flo_paths[i])
+        img1 = cv.resize(img1,(256,256))
+        img2 = cv.resize(img2,(256,256))
+        flo = cv.resize(flo,(256,256))
         if self.transform is not None:
             img1,img2,flo = self.transform(img1,img2,flo)
         return img1, img2, flo
